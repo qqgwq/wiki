95ff32adb25f000592d53a5062573c54fde7ced4
gwq <1305851004@qq.com>
2017-03-23 10:33:40 +0800

    delete development

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ env :PATH, ENV['PATH']
 env :GEM_PATH, ENV['GEM_PATH']
 set :output, {:error => '~/Desktop/cron.error.log', :standard => '~/Desktop/cron.standard.log'}
 every 1.day, at: '10:30 am' do
-  runner "PersonJob.perform_later", :environment => :development
+  runner "PersonJob.perform_later"
 end
