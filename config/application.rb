45fc338c6a6492d16db7d57f8564bc14f0e6e80e
gwq <1305851004@qq.com>
2017-03-23 14:48:52 +0800

    update date

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Jenkin
     config.i18n.default_locale = "zh-CN"
     config.active_job.queue_adapter = :sidekiq
     CONFIG = Rails.application.secrets
-    config.time_zone = 'Shanghai'
+    config.time_zone = 'Beijing'
     config.active_record.default_timezone = :local
   end
 end
