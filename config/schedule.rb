039cad05386233316d5e3d59aeda9ba686293a25
gwq <1305851004@qq.com>
2017-03-23 13:38:24 +0800

    Update schedul

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-env :PATH, ENV['PATH']
-env :GEM_PATH, ENV['GEM_PATH']
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+
 set :output, {:error => '~/Desktop/cron.error.log', :standard => '~/Desktop/cron.standard.log'}
-every 1.day, at: '10:45 am' do
+every 1.day, at: '13:42 pm' do
   runner "PersonJob.perform_later"
 end
