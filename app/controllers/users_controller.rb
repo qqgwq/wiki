b47815ef171f0116951467f8f379d5b25d236e20
gwq <1305851004@qq.com>
2017-03-23 16:38:11 +0800

    nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,11 +41,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    if @user.destroy then
+     @user.destroy 
      redirect_to root_path
-    else
-     render :edit
-    end
   end
 
   private
