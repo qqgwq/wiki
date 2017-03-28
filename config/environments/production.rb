43d06b654a4ee8bdf0c3fd27375cd6b218ff62ef
gwq <1305851004@qq.com>
2017-03-28 15:13:22 +0800

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
