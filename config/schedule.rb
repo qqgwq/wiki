00176aca59ac857aca8e49955fbdc938cfc63c4e
gwq <1305851004@qq.com>
2017-04-21 09:43:37 +0800

    update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 
 set :output, {:error => 'log/cron_error.log', :standard => 'log/cron.log'}
-every 1.day, at: '8:30 am' do
+every 1.day, at: '9:46 am' do
   runner "PersonJob.perform_later"
 end
