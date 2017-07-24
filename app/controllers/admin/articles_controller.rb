79a028f6476dcc542008066b0586a6ec9d51e817
gwq <1305851004@qq.com>
2017-07-24 14:35:13 +0800

    update admin/articles_controller.rb

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -10,15 +10,19 @@ class Admin::ArticlesController < Admin::ApplicationController
 
   def complete
     @article = Article.find(params[:id])
-    @article.complete! if @article.audit?
-    flash[:success] = "审核通过"
-    redirect_to admin_articles_path
+    if @article.audit? || @article.deny?
+      @article.complete! 
+      flash[:success] = "审核通过"
+      redirect_to admin_articles_path
+    end
   end
 
   def deny
     @article = Article.find(params[:id])
-    @article.deny! if @article.audit?
-    flash[:danger] = "已拒绝"
-    redirect_to admin_articles_path
+    if @article.audit? || @article.complete?
+      @article.deny! 
+      flash[:danger] = "已拒绝"
+      redirect_to admin_articles_path
+    end
   end
 end
