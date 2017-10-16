deaeb4de1552a03529e6c6e379b8da8371f81f54
gwq <1305851004@qq.com>
2017-10-16 16:01:24 +0800

    remove dependent_all

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   attr_accessor :comment_id
   belongs_to :article, counter_cache: true
   belongs_to :user
-  has_many :notifications, as: 'subject', dependent: :delete_all
+  has_many :notifications, as: 'subject'
   validates :content, presence: true
 end
