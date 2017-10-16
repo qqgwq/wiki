29c9554060459ceb78a9f7a9f797cbbd8d8b69c4
gwq <1305851004@qq.com>
2017-10-16 16:47:10 +0800

    add dependent_all

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   attr_accessor :comment_id
   belongs_to :article, counter_cache: true
   belongs_to :user
-  has_many :notifications, as: 'subject'
+  has_many :notifications, as: 'subject', dependent: :delete_all
   validates :content, presence: true
 end
