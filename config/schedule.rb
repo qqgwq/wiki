e50b598968a5d0375dacb7ff92c2fd519191caa0
gwq <1305851004@qq.com>
2018-07-09 08:42:11 +0800

    cancel every day send sms

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 env :PATH, ENV['PATH']
 
-set :output, {:error => 'log/cron_error.log', :standard => 'log/cron.log'}
-every 1.day, at: '8:30 am' do
-  runner "PersonJob.perform_later"
-end
+# set :output, {:error => 'log/cron_error.log', :standard => 'log/cron.log'}
+# every 1.day, at: '8:30 am' do
+#   runner "PersonJob.perform_later"
+# end
 
 #每天凌晨清理缓存
 set :output, {:error => 'log/cron_error.log', :standard => 'log/cron.log'}
