6b721fff106171f4ed8eb6f10356292f992b715b
gwq <1305851004@qq.com>
2018-12-27 11:13:19 +0800

    delete ......

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,7 +63,7 @@ javascript:
   });
 
   //sidebar
-  $(document).ready(function(){
+  $(document).on("turbolinks:load", function(){
     $('#menu-toggle').click(function(e){
       e.preventDefault();
       $('#wrapper').toggleClass('toggled');
