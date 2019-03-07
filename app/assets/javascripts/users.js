527385dfd738e5ad3306d444e5b23d2d7d2195cf
gwq <1305851004@qq.com>
2019-03-07 08:01:37 +0800

    fixed greetings

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -63,33 +63,3 @@ $(document).on('turbolinks:load', function(){
     },1000);
   });
 })
-
-$(document).ready(function(){
-  now = new Date()
-  hour = now.getHours()
-  if(hour < 6){
-    $('.greetings').html("凌晨好！")
-  }
-  else if (hour < 9){
-    $('.greetings').html("早上好！")
-  } 
-  else if (hour < 12){
-    $('.greetings').html("上午好！")
-  } 
-  else if (hour < 14){
-    $('.greetings').html("中午好！")
-  } 
-  else if (hour < 17){
-    $('.greetings').html("下午好！")
-  } 
-  else if (hour < 19){
-    $('.greetings').html("傍晚好！")
-  } 
-  else if (hour < 22){
-    $('.greetings').html("晚上好！")
-  } 
-  else {
-    $('.greetings').html("夜里好！")
-  }
-});
-
