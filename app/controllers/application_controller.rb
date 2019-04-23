63a4baf7a9764b891fc4a2b376aabbb4193a4589
gwq <1305851004@qq.com>
2019-04-23 08:26:49 +0800

    fixed csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true
   before_action :set_raven_context
   helper_method :login?, :current_user, :right_user, :correct_user, :require_is_admin
   include ExceptionHandler
