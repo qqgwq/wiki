0e2cf256ad42301b2ad833bab45d45796c4eb474
gwq <1305851004@qq.com>
2019-06-07 09:12:21 +0800

    changed chechk time

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,6 +16,6 @@ end
 set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 1.day, at: '9:00 am' do
+every 1.day, at: '9:30 am' do
   rake "lunar_date:all_lunar"
 end
